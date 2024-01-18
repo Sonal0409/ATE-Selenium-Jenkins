861657127168a81f275dfcf50273840b8a9495e1
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2024-01-18 11:15:36 +0530

    Update Seleniumtest1.java

--- src/main/java/seleniumjenkins/Seleniumtest1.java
+++ src/main/java/seleniumjenkins/Seleniumtest1.java
@@ -12,37 +12,11 @@ public class Seleniumtest1 {
 	public void method1() throws InterruptedException
 	{
 
-		
 		WebDriver driver = new ChromeDriver();
-		
-		// Maximize the browser window
-		
 		driver.manage().window().maximize();
-		
-		//1. Open the url on the browser
-		
-		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
-		
-		driver.findElement(By.xpath("//div[@class='floatL leftwidth']/descendant::input[3]")).click();
-		
-		Thread.sleep(3000);
-		
-		//Switch from current window to Alert box
-		
-		// Alert is a class in selenium, responsible to handle alert box element
-		
-		Alert a= driver.switchTo().alert();
-		
-		System.out.println(	a.getText());
-		
-		Thread.sleep(2000);
-		
-		a.accept();  // click on ok or Yes
-		
-		//a.dismiss();  // click on NO, cancel button on the alert box
-		
-	driver.findElement(By.xpath("//div[@class=\"table\"]/descendant::input[1]")).clear();
-		driver.findElement(By.xpath("//div[@class=\"table\"]/descendant::input[1]")).sendKeys("sonal");	
+		driver.get("https://www.google.com/");
+		System.out.println("The title of the page is :" + driver.getTitle());
+		driver.close();
 	
 
 	}
