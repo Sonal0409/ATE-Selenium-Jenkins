c2dbdc405bde791f72992e70f174e44450c7b13c
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2025-02-24 01:12:38 +0530

    Update Seleniumtest1.java

--- src/main/java/seleniumjenkins/Seleniumtest1.java
+++ src/main/java/seleniumjenkins/Seleniumtest1.java
@@ -1,22 +1,21 @@
 package seleniumjenkins;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+
 
 public class Seleniumtest1 {
 	
 	@Test
 	public void method1() throws InterruptedException
 	{
-
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.google.com/");
-		System.out.println("The title of the page is :" + driver.getTitle());
-		driver.close();
+		System.out.println("Open the Chrome browser");
+		//WebDriver driver = new ChromeDriver();
+		//driver.manage().window().maximize();
+		System.out.println("Maxamise the browser");
+		System.out.println("Open the URL");
+		//driver.get("https://www.google.com/");
+		System.out.println("test the feature"));
+		
+		System.out.println("Close the browser"));
 	
 
 	}
